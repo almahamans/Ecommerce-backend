b4f185253cb9f64a0c2612143b7a6f74ee6078e5
Rehab-alyahya <441021011@sm.imamu.edu.sa>
2024-09-26 15:01:57 +0300

    database connection

--- EFCore/AppDBContext.cs
+++ EFCore/AppDBContext.cs
@@ -4,13 +4,13 @@ using Microsoft.EntityFrameworkCore;
 public class AppDBContext : DbContext
 {
 
-    public AppDBContext(DbContextOptions <AppDBContext> options) : base(options)
-    {}
-    // all the entites
+  public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+  { }
+  // all the entites
   // Ex  public DbSet<UserDto> Users {get; set;}
-protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        //ex
-      //  modelBuilder.Entity<UserDto>(entity =>{ });
-    }
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    //ex
+    //  modelBuilder.Entity<UserDto>(entity =>{ });
+  }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,14 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
- 
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDBContext>(options =>
- options.UseNpgsql(builder.Configuration.GetConnectionString
- ("DefaultConnection")));
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+// builder.Services.AddDbContext<AppDBContext>(options =>
+//  options.UseNpgsql(builder.Configuration.GetConnectionString
+//  ("DefaultConnection")));
 
 var app = builder.Build();
 
