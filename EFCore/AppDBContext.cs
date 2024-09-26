6800f813378cd5edd10edae86084ed068befeca6
shahdu <shahadmoalzoman@gmail.com>
2024-09-26 15:07:05 +0300

    Adding API Controllers

--- EFCore/AppDBContext.cs
+++ EFCore/AppDBContext.cs
@@ -4,13 +4,14 @@ using Microsoft.EntityFrameworkCore;
 public class AppDBContext : DbContext
 {
 
-    public AppDBContext(DbContextOptions <AppDBContext> options) : base(options)
-    {}
+    public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+    { }
     // all the entites
-  // Ex  public DbSet<UserDto> Users {get; set;}
-protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // public DbSet<UserDto> Users { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         //ex
-      //  modelBuilder.Entity<UserDto>(entity =>{ });
+        //  modelBuilder.Entity<UserDto>(entity =>{ });
+
     }
 }
